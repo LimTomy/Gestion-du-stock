63da553751d60c082fee539d9128f033b41e847d
LimTomy <tommy.limier@gmail.com>
2025-04-01 15:11:27 -0400

    Delete ViderListe.js

--- ViderListe.js
+++
@@ -1,3 +0,0 @@
-// Vider le contenu de l'élément select pour éviter les doublons
-    var x = document.getElementById("selecteur-fruit");
-    x.innerHTML = "<option value=''>Rechercher dans le stock ?</option>";
